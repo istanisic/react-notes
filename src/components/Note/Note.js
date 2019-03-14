2c879e512b22a5a3f6059b14ba6fbc84ef214185
istanisic <ivanstanisic1@gmail.com>
2019-03-13 22:24:11 -0400

    Third commit
    Added delete button

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Note.css';
+import DeleteButton from '../DeleteButton/DeleteButton';
 
 class Note extends React.Component {
 
   handleChange = (e) => {
-    e.target.id === "title" ? this.props.onChange(this.props.id, e.target.value) : this.props.onChange(this.props.id, this.props.title, e.target.value);
+    e.target.id === "title" ? this.props.onChange(this.props.id, e.target.value, this.props.description) : this.props.onChange(this.props.id, this.props.title, e.target.value);
   }
 
   render() {
     return (
       <div className="Note">
-        <input id="title" type="text" value={this.props.title} onChange={this.handleChange} />
-        <input id="desc" type="text" value={this.props.description} onChange={this.handleChange} />
+        <input id="title" type="text" value={this.props.title} onChange={this.handleChange} placeholder="Title" />
+        <textarea rows="5" id="desc" type="text" value={this.props.description} onChange={this.handleChange} placeholder="Description" />
+        <DeleteButton id={this.props.id} onDelete={this.props.onDelete} />
       </div>
     );
   }
--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -6,7 +6,7 @@ class Notes extends React.Component {
   render() {
     return (
       this.props.notes.map((note, index) => {
-        return <Note id={note.id} title={note.title} description={note.description} onChange={this.props.onChange} />;
+        return <Note id={note.id} title={note.title} description={note.description} onChange={this.props.onChange} onDelete={this.props.onDelete} />;
       })
     );
   }
